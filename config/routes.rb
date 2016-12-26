8f358931a69500e89885ff306494558c25a6abd8
tomii <g244103@outlook.com>
2016-12-26 17:25:32 +0530

    Add ability to create books

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+
   root 'home#land'
+  resources :books, only: [:index, :new, :create, :show]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
