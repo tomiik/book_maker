674b63ed78f84a097dde0014b7e30844fcb351cc
tomii <g244103@outlook.com>
2016-12-26 17:58:39 +0530

    Add ability to delete a book with confirmation

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root 'home#land'
-  resources :books, only: [:index, :new, :create, :show]
+  resources :books, only: [:index, :new, :create, :show, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
