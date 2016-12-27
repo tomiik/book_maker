9bce2cfe01326f98b84a25e41ec7d9a1b74175af
tomii <g244103@outlook.com>
2016-12-27 14:58:28 +0530

    Added ability to edit the content of a section

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,18 +1,30 @@
 class SectionsController < ApplicationController
   before_action :fetch_chapter
+  before_action :fetch_section, only: [:show, :update]
 
   def create
     @section = @chapter.sections.new(section_params)
     @save_success = @section.save
   end
 
+  def show
+  end
+
+  def update
+    @section_update = @section.update_attributes(section_params)
+  end
+
   private
+    def fetch_section
+      @section = @chapter.sections.find(params[:id])
+    end
+
     def fetch_chapter
       @book = Book.find(params[:book_id])
       @chapter = @book.chapters.find(params[:chapter_id])
     end
 
     def section_params
-      params.require(:section).permit(:title, :notes)
+      params.require(:section).permit(:title, :notes, :content)
     end
 end
